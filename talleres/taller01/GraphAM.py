cd5ece9a65a70698e30b87586b6ce9c9b1e1f429
Maria Alejandra Vélez Clavijo <68791969+malejav02@users.noreply.github.com>
2022-01-24 10:21:11 -0500

    Create GraphAM.py

---
+++ talleres/taller01/GraphAM.py
@@ -0,0 +1,16 @@
+class GraphAm:
+
+    def __init__(self, size):
+
+
+    def getEdges(self):
+
+
+    def getWeight(self, source, destination):
+
+
+    def addArc(self, source, destination, weight):
+
+    def getSuccessors(self, vertex):
+
+    def __str__(self):
