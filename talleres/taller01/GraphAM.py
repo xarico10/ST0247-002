437bc9850f6ac1c0adfb71cf601c9549791faed1
Maria Alejandra Vélez Clavijo <68791969+malejav02@users.noreply.github.com>
2022-01-25 14:03:19 -0500

    Update GraphAM.py

--- talleres/taller01/GraphAM.py
+++ talleres/taller01/GraphAM.py
@@ -1,16 +1,21 @@
+# Implementación de grafos con matrices de adyacencia
 class GraphAm:
-
+    #Constructor de la clase
+    #Size:  numero de vertices del grafo
     def __init__(self, size):
-
-
-    def getEdges(self):
-
+        self.matriz = np.zeros((size,size))
 
     def getWeight(self, source, destination):
+        return self.matriz[source][destination]
 
 
     def addArc(self, source, destination, weight):
+        self.matriz[source][destination]=weight
 
     def getSuccessors(self, vertex):
+        succesors=[]
+        for j in range(size):
+            if self.matriz[vertex][j]!= np.inf:
+                succesors.append(matriz[vertex][j])
+        return succesors 
 
-    def __str__(self):
