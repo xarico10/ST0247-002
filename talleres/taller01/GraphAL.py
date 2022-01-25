4eca43582972178f82395742f9603d609ba6e89d
Maria Alejandra Vélez Clavijo <68791969+malejav02@users.noreply.github.com>
2022-01-25 14:12:57 -0500

    Update GraphAL.py

--- talleres/taller01/GraphAL.py
+++ talleres/taller01/GraphAL.py
@@ -1,14 +1,24 @@
+#Implementación de grafo con lista de adyacencia
 class GraphAL:
-    def __init__(self, size):
-
-
-    def addArc(self, vertex, edge, weight):
-
-
+    def __init__(self,size):
+        self.arreglo=[0]*size
+        for i in range(size):
+            arreglo[i]=deque()
+            
+    def addArc(self,vertex,edge,weigth):
+        fila=self.arreglo[vertex]
+        parejaDestinoPeso=(edge,weigth)
+        fila.append(parejaDestinoPeso)
+        
     def getSuccessors(self, vertice):
-
-
+        fila=self.matriz[vertice]
+        succesors=[]
+        for deque in fila:
+            succesors.append(deque)
+        return succesors
+    
     def getWeight(self, source, destination):
-
-
-    def __str__(self):
+        fila=self.matriz[source]
+        for deque in fila:
+            if fila[deque][0]==destination:
+                return fila[deque][1]
