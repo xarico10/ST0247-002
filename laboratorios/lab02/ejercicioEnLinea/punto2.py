30c62639d3dbb33222634ef7ad399f23f5a30aea
Agustin <agustinricop@gmail.com>
2022-02-27 21:06:19 -0500

    lab 2 punto 2

--- laboratorios/lab02/ejercicioEnLinea/punto2.py
+++ laboratorios/lab02/ejercicioEnLinea/punto2.py
@@ -1,72 +1,55 @@
 import numpy as np
 from itertools import permutations
+from itertools import chain
 
 def leerEntrada(nombreArchivo):
-    archivo = open(nombreArchivo)
-    numeroReinas = int(archivo.readline())
-    entradas = []
-    while numeroReinas != 0:
-        #Diccionario de casillas prohibidas
-        casillasProhibidas = {}
-        #Leer todas las filas del tablero
-        for indexFila in range(numeroReinas):
-            fila = archivo.readline()
-            if '*' in fila: #Hay por lo menos un asterísco en la fila actual
-                casillasProhibidasFila = []
+  archivo = open(nombreArchivo)
+  numeroReinas = int(archivo.readline())
+  entradas = []
+  while numeroReinas != 0:
+    #Diccionario de casillas prohibidas
+    casillasProhibidas = {}
+    #Leer todas las filas del tablero
+    for indexFila in range(numeroReinas):
+      fila = archivo.readline()
+      if '*' in fila: #Hay por lo menos un asterísco en la fila actual
+        casillasProhibidasFila = []
                 #Se almacena la posición de todos los asteríscos de la fila en casillasProhibidasFila
-                for index,caracter in zip(range(len(fila)),fila):
-                    if caracter == '*':
-                        casillasProhibidasFila.append(index)
-                casillasProhibidas[indexFila] = casillasProhibidasFila
-        entradas.append([numeroReinas,casillasProhibidas])
-        print(entradas)  
-        numeroReinas = int(archivo.readline())
-    archivo.close() 
-    print(entradas)    
+        for index,caracter in zip(range(len(fila)),fila):
+          if caracter == '*':
+            casillasProhibidasFila.append(index)
+        casillasProhibidas[indexFila] = casillasProhibidasFila
+    entradas.append([numeroReinas,casillasProhibidas])
+    numeroReinas = int(archivo.readline())
+  archivo.close() 
+  return entradas
 
-def seAtacanHastaI(tablero,i):
-  for j in range(i + 1):
-     for k in range(j + 1,i + 1):
-        if abs(tablero[j] - tablero[k]) == abs(j - k) or tablero[j] == tablero[k]:
-           return True
-  return False
+def permutacionEsValida(nReinas,permutacion,casillasProhibidasTablero):
+  for i in range(nReinas):
+    if i in casillasProhibidasTablero.keys():
+      if casillasProhibidasTablero[i] == [permutacion[i]]:
+        return False
+    for j in range(i + 1,nReinas):
+      if abs(permutacion[i] - permutacion[j]) == abs(i - j):
+        return False
+  return True
 
-def nReinasCasillasProhibidas(n:int):
-	#nreinasAuxPrint(n,0,[0]*n)
-    l = []
-    nReinasAux(n,0,[0]*n,l)
-    return l
-
-def nReinasAux(n:int,c:int,t:list,l:list):
-  if c == n:
-    l.append(t)
-    print(t)
-    #print(l)
-    return
-  else:
-    for f in range(n):
-      t[c] = f
-      if not seAtacanHastaI(t,c):
-        nReinasAux(n,c + 1,t,l)
-        
-
-#def nReinasCasillasProhibidasBT(arrayEntrada):
+def nReinasCasillasProhibidas(arrayEntrada):
+  for entrada,entradaIndex in zip(arrayEntrada,range(len(arrayEntrada))):
+    nSoluciones = 0
+    nReinas = entrada[0]
+    casillasProhibidasTablero = entrada[1]
+    permutaciones = permutations(range(nReinas))
+    for permutacion in permutaciones:
+      permutacionValida = permutacionEsValida(nReinas,permutacion,casillasProhibidasTablero)
+      if permutacionValida:
+        nSoluciones = nSoluciones + 1
+    print('Case ' + str(entradaIndex + 1) + ': ' + str(nSoluciones))
+      
 
 
-
-# def nReinasCasillasProhibidas(arrayEntrada):
-#     for entrada in arrayEntrada:
-#         nReinas = entrada[0]
-#         casillasProhibidas = entrada[1]
-#         permutaciones
-
+def ejecucionEjemplo():
+  arrayEntrada = leerEntrada('archivoPrueba.txt')
+  nReinasCasillasProhibidas(arrayEntrada)
     
-
-
-
-
-
-def main():
-    nReinasCasillasProhibidas(4)
-    
-main()
+ejecucionEjemplo()
